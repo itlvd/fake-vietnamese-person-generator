ed0a1f6b8d43d912001cd2f1e3fa2c27a4ab3325
Lê Văn Đông <itlvd@outlook.com>
2022-03-31 10:28:17 +0700

    change random 10 person to random 1 person

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
     srand(time(NULL));
     
-    for(int i = 0; i < 10; i++){
+    for(int i = 0; i < 1; i++){
         Person p1;
         p1.next();
         cout << p1.getInfoPerson() << endl << "=================================================\n";
